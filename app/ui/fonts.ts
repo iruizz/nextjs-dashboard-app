cccf9d298451b15edae93b387c0723aaad257682
iruizz <98923057+iruizz@users.noreply.github.com>
2023-10-29 16:21:45 -0500

    First commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Lusitana } from 'next/font/google';
+//Import the Inter font from the next/font/google module - this will be your primary font.
+
+//specify what subset you’d like to load. In this case, 'latin'
+export const inter = Inter({subsets: ['latin']});
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
